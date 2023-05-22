fa3aca5bcf9a14a25c87f702c070cc633258c79a
Sebastian <sebassd@gmail.com>
2023-05-21 21:33:03 -0400

    Adding JS - DOMContentLoaded

--- app-script.js
+++ app-script.js
@@ -1,1 +1,11 @@
-
+// Using DOMContentLoaded for assigning EventListener to buttons 
+document.addEventListener('DOMContentLoaded', function() {
+    const searchButton = document.getElementById('searchButton');
+    const clearButton = document.getElementById('clearButton');
+    const randomButton = document.getElementById('randomButton');
+  
+    searchButton.addEventListener('click', searchCharacters);
+    clearButton.addEventListener('click', clearCharacters);
+    randomButton.addEventListener('click', getRandomCharacter);
+  });
+  
